fe66f6af00cebdb33ac1e4955b48ec3dd7085d56
juanancho2 <juan.uzuriaga@gmail.com>
2019-08-01 20:20:26 -0500

    Componentes

---
+++ weather-app/src/Components/Location.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Location = () => {
+   return (<div><h1>Cali</h1></div>);
+};
+
+export default Location;
---
+++ weather-app/src/Components/WeaterData.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WeatherData = () => {
+   return (<div>Weather data</div>);
+};
+
+export default WeatherData;
