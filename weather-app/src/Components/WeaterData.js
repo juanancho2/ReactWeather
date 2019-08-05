d53f116d643d9060b2d614b097df3d3950ddedd8
juanancho2 <juan.uzuriaga@gmail.com>
2019-08-04 20:38:44 -0500

    Properties

--- weather-app/src/Components/WeaterData.js
+++ weather-app/src/Components/WeaterData.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Extrainfo from './WeatherExtraInfo';
+import Temp from './WeatherTemperature';
 
-const WeatherData = () => {
-   return (<div>Weather data</div>);
-};
+const WeatherData = () => (
+            <div>
+                <Temp></Temp> 
+                <Extrainfo humidity={80} wind={"20 m/s"}></Extrainfo>
+            </div>);
+
 
 export default WeatherData;
